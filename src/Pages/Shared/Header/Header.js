89bb5d5e59d2e7113a70d50f40b81ddcc60c5a71
saba rahman <sabarahman.official@gmail.com>
2022-04-16 23:05:04 +0600

    pages created

---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+import { HashLink } from 'react-router-hash-link';
+
+const Header = () => {
+    // const { user, logOut } = useAuth();
+    return (
+        <>
+            <Navbar bg="dark" variant="dark" sticky="top" collapseOnSelect expand="lg" >
+                <Container>
+                    <Navbar.Brand href="#home">Code With Saba</Navbar.Brand>
+                    <Navbar.Toggle />
+                    <Navbar.Collapse className="justify-content-end">
+                        <Nav.Link as={HashLink} to="/home#home">Home</Nav.Link>
+                        <Nav.Link as={HashLink} to="/home#services">Services</Nav.Link>
+                        <Nav.Link as={HashLink} to="/blogs">Blogs</Nav.Link>
+
+                        {/* {user?.email ?
+                            <Button onClick={logOut} variant="light">Logout</Button> :
+                            <Nav.Link as={Link} to="/login">Login</Nav.Link>}
+                        <Navbar.Text>
+                            Signed in as: <a href="#login">{user?.displayName}</a>
+                        </Navbar.Text> */}
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    );
+};
+
+export default Header;
